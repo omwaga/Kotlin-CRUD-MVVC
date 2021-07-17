2580427891def842879bd31ef96e293921aa4a1b
Collins Omwaga <collinsomwaga@gmail.com>
2021-07-17 15:28:51 +0300

    Working Dao

---
+++ app/src/main/java/com/example/kotlinroom_crud/SubscriberViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinroom_crud
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.kotlinroom_crud.db.SubscriberRepository
+
+class SubscriberViewModelFactory(
+    private val repository: SubscriberRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(SubscriberViewModel::class.java)){
+            return SubscriberViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown View Model class")
+    }
+
+}
